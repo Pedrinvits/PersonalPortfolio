e580234e9aa647e64b681af06852b263814aa969
pedrinvits <pedromantovani26@gmail.com>
2024-02-24 13:11:40 -0300

    fix : scroll bar em links

--- app/components/Link/index.tsx
+++ app/components/Link/index.tsx
@@ -5,7 +5,7 @@ type LinkProps = ComponentProps<typeof NextLink>
 
 export const Link = ({className, children, ...props} : LinkProps) => {
     return (
-        <NextLink target="_blank" className={cn(
+        <NextLink className={cn(
             "flex items-center gap-2 text-gray-300 text-sm hover:text-light-purple transition-colors",
             className
         )} {...props}>
