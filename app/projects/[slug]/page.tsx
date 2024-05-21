ebab3cd85f7724c1d94371a8b6f135853f28890a
Pedro Mantovani <pedro.mantovani@contabilivre.com>
2024-05-21 11:43:09 -0300

    fix generate meta data

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -73,22 +73,22 @@ export async function generateStaticParams() {
   return projects
 }
 
-export async function generateMetadata({
-  params : { slug }
-} : ProjectProps) : Promise<Metadata> {
-  const data = await getProjectDetails(slug)
-  const project = data.project
-  return {
-    title : project.title,
-    description : project.description.text,
-    openGraph : {
-      images : [
-        {
-          url : project.thumbnail.url,
-          width : 1200,
-          height : 630,
-        }
-      ]
-    }
-  }
-}
+// export async function generateMetadata({
+//   params : { slug }
+// } : ProjectProps) : Promise<Metadata> {
+//   const data = await getProjectDetails(slug)
+//   const project = data.project
+//   return {
+//     title : project.title,
+//     description : project.description.text,
+//     openGraph : {
+//       images : [
+//         {
+//           url : project.thumbnail.url,
+//           width : 1200,
+//           height : 630,
+//         }
+//       ]
+//     }
+//   }
+// }
